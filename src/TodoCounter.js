ee2935b38f3633a9d1ff3c1dd5f8317190da9b6b
j0seReal <94468406+j0seReal@users.noreply.github.com>
2023-10-13 20:39:06 -0700

    autocomplete by teacher

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,16 +1,11 @@
 import './TodoCounter.css';
 
-function TodoCounter({ total, completed }) { {/* props: Object */}
-    return (
-      <h1 /*style={{
-        fontSize: '24px',
-        textAlign: 'center',
-        margin: 0,
-        padding: '48px'
-      }}*/> {/* style es un objeto de objetos */}
-        Haz completado {completed} de {total} TODOS
-      </h1>
-    );
-  }
+function TodoCounter({ total, completed }) {
+  return (
+    <h1 className="TodoCounter">
+      Has completado <span>{completed}</span> de <span>{total}</span> TODOs
+    </h1>
+  );
+}
 
-  export { TodoCounter };
+export { TodoCounter };
