923fe4ea1e620080ab41b52b7e843e90c572b585
j0seReal <94468406+j0seReal@users.noreply.github.com>
2023-10-15 13:15:14 -0700

    createtodobutton

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,7 +2,12 @@ import './CreateTodoButton.css';
 
 function CreateTodoButton() {
   return (
-    <button className="CreateTodoButton">+</button>
+    <button className="CreateTodoButton" 
+            onClick={(event) => {
+              console.log('Le diste click al boton ');
+              console.log(event);
+              console.log(event.target);
+            }}>+</button>
   );
 }
 
