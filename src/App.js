32c1a5a568b28472f6b3e00eef69c14228935a60
j0seReal <94468406+j0seReal@users.noreply.github.com>
2023-10-15 21:02:26 -0700

    search todo
    search todo TERMINADO
    completeTodo PENDIENTE

--- src/App.js
+++ src/App.js
@@ -10,41 +10,51 @@ const defaultTodos = [
   { text: 'Tomar el Curso de Intro a React.js', completed: false },
   { text: 'Llorar con la Llorona', completed: false },
   { text: 'LALALALALA', completed: false },
-  { text: 'Usar estados derivados', completed: true },
+  { text: 'Usar Estados Derivados', completed: true },
 ];
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [todos, setTodos] = React.useState(defaultTodos)
   const [searchValue, setSearchValue] = React.useState('');
 
-  const completedTodos = todos.filter(
-    todo => !!todo.completed
-  ).length;
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
 
-  console.log('Los usuarios buscan todos de ' + searchValue);
+  const searchedTodos = todos.filter(
+    (todo) => {
+      const todoText = todo.text.toLocaleLowerCase();
+      const todoSearch = searchValue.toLocaleLowerCase();
+      return todoText.includes(todoSearch);
+    }
+  );
+
+  const completeTodo = (text) => {
+    const newTodos = [...todos];
+
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  }
 
   return (
     <>
-      <TodoCounter
-        completed={completedTodos}
-        total={totalTodos} 
-      />
+      <TodoCounter 
+        completed={completedTodos} 
+        total={totalTodos} />
+
       <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
+        searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
-        {defaultTodos.map(todo => (
+        {searchedTodos.map(todo => (
           <TodoItem
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onCOmplete={completeTodo}
           />
         ))}
       </TodoList>
-
+      
       <CreateTodoButton />
     </>
   );
