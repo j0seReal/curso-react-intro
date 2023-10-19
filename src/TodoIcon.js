118e23f742eb28de47fc0f602a800250f1f83710
j0seReal <94468406+j0seReal@users.noreply.github.com>
2023-10-18 20:35:22 -0700

    enable complete and delete button

--- src/TodoIcon.js
+++ src/TodoIcon.js
@@ -3,16 +3,17 @@ import { ReactComponent as DeleteSVG } from './delete.svg';
 import './TodoIcon.css';
 
 const iconTypes = {
-    "check": <CheckSVG className='Icon-svg' />,
-    "delete": <DeleteSVG className='Icon-svg' />,
+    "check": (color) => <CheckSVG className='Icon-svg' fill={color} />,
+    "delete": (color) => <DeleteSVG className='Icon-svg' fill={color} />,
 };
 
-function TodoIcon({ type }) {
+function TodoIcon({ type, color, onClick }) {
     return (
         <span
             className={`Icon-container Icon-container-${type}`}
+            onClick={onClick}
         >
-            {iconTypes[type]}
+            {iconTypes[type](color)}
         </span>
     )
 }
