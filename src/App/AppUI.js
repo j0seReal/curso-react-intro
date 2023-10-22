53b6ad8e59229fd22d875dd9be78b4d14d5a2a3b
j0seReal <94468406+j0seReal@users.noreply.github.com>
2023-10-21 18:41:27 -0700

    useEffect

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -5,6 +5,8 @@ import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from '../CreateTodoButton';
 
 function AppUI({
+    loading,
+    error,
     completedTodos,
     totalTodos,
     searchValue,
@@ -23,6 +25,9 @@ function AppUI({
             searchValue={searchValue} setSearchValue={setSearchValue} />
 
         <TodoList>
+            {loading && <p>Estamos cargando</p>}
+            {error && <p>Error</p>}
+            {(!loading && searchedTodos.length === 0) && <p>Crea tu primer TODO</p>}
             {searchedTodos.map(todo => (
             <TodoItem
                 key={todo.text}
